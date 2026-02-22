eef8f902cd2977d9193b6ac5624120c472859d24
manjula1711 <141096624+manjula1711@users.noreply.github.com>
2026-02-22 22:27:35 +0530

    Update Jobs.jsx

--- src/pages/public/Jobs.jsx
+++ src/pages/public/Jobs.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import Sidebar from "../../components/layout/Sidebar";
